9757fda181668dce18ef48846487773837d916c2
marko <marko.dimitrov96@gmail.com>
2024-07-20 16:37:03 +0200

    Added apikey, added resposne to json and added title image and explatation on website

--- Home.py
+++ Home.py
@@ -9,10 +9,18 @@ apiKey= os.environ['NASA_API']
 url = f"https://api.nasa.gov/planetary/apod?api_key={apiKey}"
 
 response = requests.get(url).json()
-st.title(response['title'])
-st.image(response['url'])
-st.write(response['explanation'])
+title = response['title']
+image_url = response['url']
+descirption = response['explanation']
 
+image_filepath = "img.png"
+requrest2 = requests.get(image_url)
+with open(image_filepath,"wb") as file:
+    file.write(requrest2.content)
+
+st.title(title)
+st.image(image_filepath)
+st.write(descirption)
 
 
 
